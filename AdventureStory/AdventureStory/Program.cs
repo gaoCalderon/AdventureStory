c15843d0ec64d0d84f9d1f63380df4b5c9d2cb51
gaoCalderon <gao.vang86@gmail.com>
2016-08-11 13:52:31 -0500

    initial

---
+++ AdventureStory/AdventureStory/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureStory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName;
+
+            Console.WriteLine("Enter your character's name ");
+            characterName = Console.ReadLine();
+
+            Console.WriteLine("Hi, {0}.[ENTER]", characterName);
+            Console.ReadLine();
+            Console.WriteLine("You'll be starting school in a week. [ENTER]");
+            Console.ReadLine();
+            Console.WriteLine("So you'll have to go to the store to get a few items. [ENTER]");
+            Console.ReadLine();
+            Console.WriteLine("What you will need is: a backpack, a new outfit, and some grocery. [ENTER]");
+            Console.ReadLine();
+            Console.WriteLine("Let's get started. [ENTER]");
+            Stores stores = new Stores();
+            stores.RunStory();
+            Console.WriteLine("{0}, you're now ready to go back to school.", characterName);
+            Console.ReadLine();
+
+
+          
+
+        }
+
+    }
+}
---
+++ AdventureStory/AdventureStory/Stores.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureStory
+{
+    class Stores
+    {
+        private string departmentStores;
+
+        public Stores()
+
+        {
+
+            departmentStores = Console.ReadLine();
+
+        }
+        public void setStoreType()
+        {
+            Console.WriteLine("Please type one of the following store, then press [Enter] key: Clothing, School Supply, or Grocery");
+            departmentStores = Console.ReadLine()
+;        }
+            public void enterDeparmentStores()
+        {
+
+            
+            switch (departmentStores)
+            {
+                case "Clothing":
+                    Console.WriteLine("Welcom to Micki's Clothing store.");
+                    Console.WriteLine("Would you like to buy an outfit for $40? Yes/No");
+                    string userchoice = Console.ReadLine();
+                    if (userchoice == "yes")
+                    {
+                        Console.Write("Congrats, you have purchased an outfit!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Don't forget, you still need an outfit for school.");
+                    }
+                    if (userchoice == "no")
+                    {
+                        Console.WriteLine("You decided not to purchase an outfit for school.");
+                    }
+                    Console.WriteLine(" Have a great day, come back soon.");
+                    break;
+
+                case "School Supply":
+                    Console.WriteLine("Welcome to Everything School Supplies.");
+                    Console.WriteLine("Would you like to buy a brand new backpack for $12? Yes/No");
+                    string userStoreChoice = Console.ReadLine();
+                    if (userStoreChoice == "yes")
+                    {
+                        Console.WriteLine("Congrats, you have purchased a brand new backpack!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Don't forget, you still need a backpack for school.");
+                    }
+                    if (userStoreChoice == "no")
+                    {
+                        Console.WriteLine("You decided not to purchase a new backpack for school.");
+                    }
+                    Console.WriteLine(" Best of Luck to you champ");
+                    break;
+
+                case "Grocery":
+                    Console.WriteLine("Welcome to The Modern Produce.");
+                    Console.WriteLine("Would you like to purchase this bag of groceries for $18? Yes/No");
+                    userchoice = Console.ReadLine();
+                    if (userchoice == "yes")
+                    {
+                        Console.WriteLine("Congrats, you have purchased a weeks worth of grocery.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Don't forget, you still need food for the week.");
+                    }
+                    if (userchoice == "no")
+                    {
+                        Console.WriteLine("You decided not to purchase grocery for the week.");
+                    }
+                    Console.WriteLine(" Thank you for choosing Modern Porduce, see you soon.");
+                    break;
+
+                default:
+                    Console.WriteLine("NOT VALID, Please pick a store between: Clothing, School Supply, or Grocery.");
+                    break;
+            }
+        }
+        public void RunStory()
+        {
+            bool keepBuying = true;
+            while (keepBuying)
+            {
+                setStoreType();
+                enterDeparmentStores();
+                Console.WriteLine("Would you like to buy more things? yes/no");
+                string options = Console.ReadLine();
+                if (options == "yes")
+                {
+                    Console.WriteLine();
+                }
+            
+            else if (options == "no")
+            {
+                keepBuying = false;
+            }
+        }
+        }
+
+     }
+}
